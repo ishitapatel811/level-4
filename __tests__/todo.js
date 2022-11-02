ee83359226143e37f88f70945f9e2fc448b55e20
Patel Ishitaben Manojbhai <113087681+ishitapatel811@users.noreply.github.com>
2022-11-02 07:44:19 -0700

    my submission Level _4

---
+++ __tests__/todo.js
@@ -0,0 +1,63 @@
+const todoList = require("../todo");
+const { all, markAsComplete, add, overdue, dueLater, dueToday } = todoList();
+
+var today = new Date();
+let yesterday = new Date(new Date().setDate(today.getDate() - 1));
+let tomorrow = new Date(new Date().setDate(today.getDate() + 1));
+
+describe("TodoList test suite", () => {
+  beforeAll(() => {
+    add({
+      title: "learing coding",
+      completed: false,
+      dueDate: yesterday.toLocaleDateString("en-CA"),
+    });
+    add({
+      title: "submit assignment",
+      completed: false,
+      dueDate: tomorrow.toLocaleDateString("en-CA"),
+    });
+  });
+  test("add new todo list", () => {
+    const todoItemsCount = all.length;
+    add({
+      title: "repair the laptoop",
+      completed: false,
+      dueDate: today.toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(todoItemsCount + 1);
+  });
+  test("Should mark todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+  test("relative all todo that are overdue", () => {
+    const overdueCount = overdue().length;
+    add({
+        title: "wating the email",
+        completed: false,
+        dueDate: yesterday.toLocaleDateString("en-CA"),
+      });
+    expect(  overdue().length  ).toBe(overdueCount+1);
+  });
+  test("relative all todo that are duetoday", () => {
+    const dueTodayCount = dueToday().length;
+    add({
+        title:"wating the submission feedback",
+        completed: false,
+        dueDate: today.toLocaleDateString("en-CA"),
+      });
+    expect(dueToday().length).toBe(dueTodayCount + 1);
+  });
+  test("relative all todo that are dueLater", () => {
+    const dueLaterCounter = dueLater().length;
+    add({
+        title: "submit my new assignments",
+        completed: false,
+        dueDate: tomorrow.toLocaleDateString("en-CA"),
+      });
+    expect(dueLater().length).toBe(dueLaterCounter + 1);
+  });
+});
+    
